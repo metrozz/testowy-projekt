5df2a727eefb4c3fb20e1f6a12a6a36abc79434d
Metrozz <metrozz.krk@gmail.com>
2020-03-16 19:15:03 +0100

    Edit: - Test.java : add method: printCommit

--- src/main/java/pl/demojanusz/testowyprojekt/Test.java
+++ src/main/java/pl/demojanusz/testowyprojekt/Test.java
@@ -1,4 +1,14 @@
 package pl.demojanusz.testowyprojekt;
 
 public class Test {
+
+    private String commit;
+
+    public Test(String commit) {
+        this.commit = commit;
+    }
+
+    public void printCommit(){
+        System.out.println(commit);
+    }
 }
