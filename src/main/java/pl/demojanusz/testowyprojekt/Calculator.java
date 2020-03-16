1a3c0ef391e2ccf8873785ddf734c548fbb7ed68
Metrozz <metrozz.krk@gmail.com>
2020-03-16 19:19:04 +0100

    add clas calcualtor

---
+++ src/main/java/pl/demojanusz/testowyprojekt/Calculator.java
@@ -0,0 +1,13 @@
+package pl.demojanusz.testowyprojekt;
+
+public class Calculator {
+
+    private int x;
+    private int y;
+
+    public Calculator(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
